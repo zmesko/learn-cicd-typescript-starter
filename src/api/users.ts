144e0b675af04daa8cf484101226b29f6d0dcf6a
zmeskoadam <zmesi66@gmail.com>
2026-03-16 15:54:11 +0100

    Fix linting issue

--- src/api/users.ts
+++ src/api/users.ts
@@ -37,6 +37,5 @@ function generateRandomSHA256Hash(): string {
   // should we be using crypto.randomBytes instead of crypto.pseudoRandomBytes?
   return crypto
     .createHash("sha256")
-    .update(crypto.pseudoRandomBytes(32))
     .digest("hex");
 }
