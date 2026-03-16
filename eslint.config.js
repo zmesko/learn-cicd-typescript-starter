8b0bf989299284450a97844f1ca0e82de5c02d29
zmeskoadam <zmesi66@gmail.com>
2026-03-16 15:42:49 +0100

    Add security check

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,18 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node },
+  },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ]);
