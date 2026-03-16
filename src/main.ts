ef9d789091bd851fad1423f7213694fc69d78f63
zmeskoadam <zmesi66@gmail.com>
2026-03-16 15:31:13 +0100

    Delete unused function

--- src/main.ts
+++ src/main.ts
@@ -47,9 +47,3 @@ app.use("/v1", v1Router);
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
-
-
-function unused() {
-  // this function does nothing
-  // and is called nowhere
-}
